de946d21405eb2ed873398f7b96ec6197d595a80
Andrew Paley <andrewpaley@gmail.com>
2018-12-11 21:06:41 -0600

    teacher fulltest flow updates

--- teacher.py
+++ teacher.py
@@ -26,34 +26,48 @@ class Teacher(object):
         self.bootstrapData()
 
     def train(self, dataset = None):
+        if not dataset: dataset = self.trainSet
         self.nlp = spacy.blank('en')
-        self.trainQuoteClassifier(self.trainSet)
+        self.trainQuoteClassifier(dataset)
 
-    def test(self):
-        results = self.testQuoteClassifier()
+    def test(self, dataset = None):
+        if not dataset: dataset = self.testSet
+        results = self.testQuoteClassifier(dataset)
         self.classifySomeSnippets()
         return results
 
-    def tenLoop(self, batchSize=1, dropout=0.3):
-        self.batchSize = batchSize
-        self.dropout = dropout
+    def tenFoldCV(self, batchSize=1, dropout=0.3):
+        if not isinstance(batchSize, int) and len(batchSize.split(" ")) > 1:
+            bs = batchSize.split(" ")
+            self.batchStart = bs[1]
+            self.batchEnd = bs[2]
+            self.batchSize = bs[0]
+        else:
+            self.batchSize = batchSize
+        if not isinstance(dropout, float) and len(dropout.split(" ")) > 1:
+            do = dropout.split(" ")
+            self.dropStart = do[1]
+            self.dropEnd = do[2]
+            self.dropout = do[0]
+        else:
+            self.dropout = dropout
         # train and test ten times in a row, recording accuracy scores for each
         results = [] # will be list of ten lists: [precision, recall, f_score]
-        for i in range(0,10):
-            self.train()
-            results.append(self.test())
-            self.bootstrapData()
+        dataset = self.jdb.getAll("snippets")
+        datasets = self.prepData10CV(dataset)
+        for ds in datasets:
+            self.train(ds[0])
+            results.append(self.test(ds[1]))
         return results
 
     def bootstrapData(self):
         dataset = self.jdb.getAll("snippets")
         trainSet, testSet = self.prepData(dataset, True)
-        # set apart in case we want to do different types of analysis later...let's prep for spacy for now
         self.trainSet = self.makeSpacyReadySchema(trainSet)
         self.testSet = self.makeSpacyReadySchema(testSet)
 
     def prepData(self, dataset, testMode=True): # if testMode is false, all data is training data
-        cutoff = 0.75 # the line in the full set between train and test sets
+        cutoff = 0.8 # the line in the full set between train and test sets
         shuffle(dataset)
         # ensure quotes are equally distributed in result set
         quotes = []
@@ -81,6 +95,46 @@ class Teacher(object):
         self.testSet = testSet
         return trainSet, testSet
 
+    def prepData10CV(self, dataset):
+        shuffle(dataset)
+        # ensure quotes are equally distributed in result set
+        tenSets = [] # ultimately a list of lists of each of ten chunks
+        preppedData = [] # a list of ten tuples of train data and test data made from tenSets
+        quotes = []
+        nonquotes = []
+        for d in dataset:
+            d["training_snippet"] = self.cleaner.createRepresentation(d["snippet"])
+            if d["is_quote"] == 1:
+                quotes.append(d)
+            else:
+                nonquotes.append(d)
+        shuffle(quotes)
+        shuffle(nonquotes)
+        # swizzle the schema
+        quotes = self.makeSpacyReadySchema(quotes)
+        nonquotes = self.makeSpacyReadySchema(nonquotes)
+        # create the subsets
+        tenSetsQuotes = self.chunkList(quotes)
+        tenSetsNonQuotes = self.chunkList(nonquotes)
+        for i, payload in enumerate(tenSetsQuotes):
+            fullSet = payload + tenSetsNonQuotes[i]
+            shuffle(fullSet)
+            tenSets.append(fullSet)
+        for i, testSet in enumerate(tenSets):
+            trainSet = [snippet for chunk in tenSets if chunk!=tenSets[i] for snippet in chunk]
+            preppedData.append((trainSet, testSet))
+        self.cvSets = preppedData
+        return self.cvSets
+
+    def chunkList(self, payload):
+        n = math.floor(len(payload)/10.0)
+        output = []
+        for x in range(0, len(payload), n):
+            output.append(payload[x:x+n])
+        if len(output) > 10:
+            output = output[:-1]
+        return output
+
     def makeSpacyReadySchema(self, dataset):
         # output list of tuples like (u"snippet text", {"cats": {"QUOTE": 0}}) where quote is 0 or 1
         output = []
@@ -103,14 +157,18 @@ class Teacher(object):
         textCat.add_label("QUOTE")
         losses = {}
         if self.batchSize == "compounding":
-            batches = minibatch(trainSet, size=compounding(4., 32., 1.001))
+            start = float(self.batchStart)
+            end = float(self.batchEnd)
+            batches = minibatch(trainSet, size=compounding(start, end, 1.001))
         else:
             batches = minibatch(trainSet, size=self.batchSize)
         self.nlp.vocab.vectors.name = "quoteCategorizer"
         optimizer = self.nlp.begin_training()
         # train!
         if self.dropout == "decaying":
-            dropoutSet = decaying(0.3, 0.2, 1e-4)
+            start = float(self.dropStart)
+            end = float(self.dropEnd)
+            dropoutSet = decaying(start, end, 1e-4)
         else:
             dropout = self.dropout
         for batch in batches:
@@ -123,7 +181,7 @@ class Teacher(object):
         self.textCat = textCat
 
     def testQuoteClassifier(self, testSet=None):
-        # thanks to spacy's on-site tutorial for chunks of this function
+        # thanks to spacy's on-site tutorial for a chunk of this function
         if not testSet: testSet = self.testSet
         snippets, cats = zip(*testSet)
         if not self.losses:
@@ -131,26 +189,29 @@ class Teacher(object):
         else:
             print('{:^5}\t{:^5}\t{:^5}\t{:^5}'.format("Loss", "Pre.", "Rec.", 'F SCORE'))
         snippets = (self.nlp.tokenizer(snippet) for snippet in snippets)
-        tp = 0.0   # True positives
-        fp = 1e-8  # False positives
-        fn = 1e-8  # False negatives
-        tn = 0.0   # True negatives
+        truePos = 0.0
+        falsePos = 1e-8
+        falseNeg = 1e-8
+        trueNeg = 0.0
         for i, doc in enumerate(self.textCat.pipe(snippets)):
-            gold = cats[i]["cats"]
+            truth = cats[i]["cats"]
             for label, score in doc.cats.items():
-                if label not in gold:
+                if label not in truth:
                     continue
-                if score >= 0.5 and gold[label] >= 0.5:
-                    tp += 1.
-                elif score >= 0.5 and gold[label] < 0.5:
-                    fp += 1.
-                elif score < 0.5 and gold[label] < 0.5:
-                    tn += 1
-                elif score < 0.5 and gold[label] >= 0.5:
-                    fn += 1
-        precision = tp / (tp + fp)
-        recall = tp / (tp + fn)
-        f_score = 2 * (precision * recall) / (precision + recall)
+                if score >= 0.5 and truth[label] >= 0.5:
+                    truePos += 1.
+                elif score >= 0.5 and truth[label] < 0.5:
+                    falsePos += 1.
+                elif score < 0.5 and truth[label] < 0.5:
+                    trueNeg += 1
+                elif score < 0.5 and truth[label] >= 0.5:
+                    falseNeg += 1
+        precision = truePos / (truePos + falsePos)
+        recall = truePos / (truePos + falseNeg)
+        if precision == 0 and recall == 0:
+            f_score = 0
+        else:
+            f_score = 2 * (precision * recall) / (precision + recall)
         if not self.losses:
             print('{0:.3f}\t{1:.3f}\t{2:.3f}'.format(precision, recall, f_score))
         else:
@@ -201,16 +262,18 @@ if __name__ == "__main__":
         t.test()
         t.saveQuoteClassifier()
     elif len(sys.argv) > 1 and sys.argv[1] in ["-fulltest"]:
-        pairs = [("compounding", "decaying"), ("compounding", 0.3), ("compounding", 0.5), (5, 0.3), (5, 0.3), (15, 0.3), (0.3, 1), (0.2, 1), (0.1, 1)]
+        pairs = [("compounding 4. 32.", "decaying 0.3 0.2"), ("compounding 4. 32.", 0.3), ("compounding 4. 32.", 0.5), (5, 0.3), (15, 0.3), (1, 0.5), (1, 0.3), (1, 0.1), (1, "decaying 0.3 0.1"), ("compounding 1. 4.", 0.2)]
         fullResults = []
         for pair in pairs:
-            results = t.tenLoop(batchSize=pair[0], dropout=pair[1])
+            results = t.tenFoldCV(batchSize=pair[0], dropout=pair[1])
             fullResults.append({
                 "dropout": pair[1],
                 "batchSize": pair[0],
                 "results": results
             })
         pp(fullResults)
+        breakpoint()
+    elif "-nonstart" in sys.argv:
         breakpoint()
     else:
         t.train()
